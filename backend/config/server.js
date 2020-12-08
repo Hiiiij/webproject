383e5eabcd87246bc145674851c20e20d5bfa5cc
beta <beta.nycz@icloud.com>
2020-12-08 20:49:06 +0100

    setting up template

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4ef579a530ac2bdb99b9e3ff4359c055'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6b2f5983-2455-4e62-a277-829aa52e78f8'
+};
